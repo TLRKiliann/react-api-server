4ff8a70e3d7728a17d7f574bc0bf73591b057d0b
TLRKiliann <philogenie@protonmail.com>
2022-09-25 18:00:00 +0200

    axios module added

--- my-api/src/App.js
+++ my-api/src/App.js
@@ -1,9 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
+const baseUrl = 'http://localhost:4000/api/notes';
+
 function App() {
+  const [notes, setNotes] = useState([]);
+  const [filterNotes, setFilterNotes] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(baseUrl)
+      .then(initialNotes => {
+        console.log("promise fullfield")
+        setNotes(initialNotes.data);
+        setFilterNotes(initialNotes.data)
+      });
+  }, []);
+
   return (
     <div className="App">
 
+
+      {notes.map(note => (
+        <div key={note.id}>
+          <p>{note.id}</p>
+          <p>{note.name}</p>
+          <p>{note.number}</p>
+        </div>
+      ))}
+
+      {filterNotes.map(note => (
+        <div key={note.id}>
+          <p>{note.id}</p>
+          <p>{note.name}</p>
+          <p>{note.number}</p>
+        </div>
+      ))}
+
+      
     </div>
   );
 }
