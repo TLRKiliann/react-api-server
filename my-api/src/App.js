343509da715951fcffc206ee5d5a2f28950196b7
TLRKiliann <philogenie@protonmail.com>
2022-09-25 19:10:58 +0200

    search Name to display phone done

--- my-api/src/App.js
+++ my-api/src/App.js
@@ -8,6 +8,8 @@ function App() {
   const [notes, setNotes] = useState([]);
   const [filterNotes, setFilterNotes] = useState([]);
 
+  const [newName, setNewName] = useState("");
+
   useEffect(() => {
     axios
       .get(baseUrl)
@@ -18,27 +20,68 @@ function App() {
       });
   }, []);
 
+  const displayNote = () => {
+    axios
+      .get(baseUrl)
+      .then(initialNotes => {
+        console.log("promise fullfield")
+        setNotes(initialNotes.data);
+        setFilterNotes(initialNotes.data)
+      });
+  };
+
+  const searchNumber = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleSearch = () => {
+    const searchNum = notes.filter(note => {
+      return note.name === newName ? `${note.name} ${note.phone}` : null;
+    })
+    if (newName === "") {
+      setNotes([])
+    } else {
+      setFilterNotes(searchNum)
+    }
+  };
+
   return (
     <div className="App">
 
+      <div className="subApp--div">
 
-      {notes.map(note => (
-        <div key={note.id}>
-          <p>{note.id}</p>
-          <p>{note.name}</p>
-          <p>{note.number}</p>
+        <div className="main--title">
+          <h1>React API</h1>
         </div>
-      ))}
 
-      {filterNotes.map(note => (
-        <div key={note.id}>
-          <p>{note.id}</p>
-          <p>{note.name}</p>
-          <p>{note.number}</p>
+        <div className="display--div">
+          <label>Display All Contacts</label>
+          <button onClick={displayNote}>Display</button>
         </div>
-      ))}
 
-      
+        <div className="display--div">
+          <label>Search Contact</label>
+          <input type="text" value={newName} onChange={searchNumber} />
+          <button onClick={handleSearch}>Search</button>
+        </div>
+
+        {filterNotes.map(note => (
+          <div key={note.id} className="filtermap--div">
+            <p>{note.name}</p>
+            <p>{note.number}</p>
+          </div>
+        ))}
+
+        {notes.map(note => (
+          <div key={note.id} className="notemap--div">
+            <p>{note.id}</p>
+            <p>{note.name}</p>
+            <p>{note.number}</p>
+          </div>
+        ))}
+
+      </div>
+
     </div>
   );
 }
