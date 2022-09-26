8149d7386f461c97473f980c36f2ad319b0d792f
TLRKiliann <philogenie@protonmail.com>
2022-09-26 17:19:22 +0200

    script ok

--- my-api/src/services/noteservices.js
+++ my-api/src/services/noteservices.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:4000/api/notes';
-//const baseUrl = 'http://localhost:3001/notes';
+//const baseUrl = 'http://localhost:4000/api/notes';
+const baseUrl = 'http://localhost:3001/notes';
 
 const getAll = () => {
   const request = axios.get(baseUrl);
