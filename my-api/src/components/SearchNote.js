1d61dd00a32100b774fab56e28a98f2f5c2c32d3
TLRKiliann <philogenie@protonmail.com>
2022-09-26 19:51:32 +0200

    SearchComponent created

--- my-api/src/components/SearchNote.js
+++ my-api/src/components/SearchNote.js
@@ -1,1 +1,12 @@
-SearchNote
+import './styles.css';
+
+const SearchNote = (props) => {
+  return (
+    <div className="notemap--div">
+      <p>{props.name} :</p>
+      <p>{props.number}</p>
+    </div>  
+  )
+}
+
+export default SearchNote;
