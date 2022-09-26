04ed712568698e1fe98495c2a49700863445676c
TLRKiliann <philogenie@protonmail.com>
2022-09-26 17:17:36 +0200

    functions ok

--- my-api/src/components/Note.js
+++ my-api/src/components/Note.js
@@ -1,1 +1,45 @@
-Note
+const Note = ({
+    note,
+    handleDelete,
+    validateNumber,
+    editNum,
+    editPhoneNumber,
+    handleChangeNumber,
+    switchEditNum
+  }) => {
+
+  const labelSwitch = note.editNum ? "Hide" : "Display";
+
+  return (
+    <div className="filtermap--div">
+      <p>{note.name}</p>
+      <p>{note.number}</p>
+      <button
+        onClick={() => handleDelete}
+      >
+        Delete
+      </button>
+
+      {note.editNum ? (
+        <div>
+          <input
+            value={editPhoneNumber}
+            onChange={handleChangeNumber} />
+          <button onClick={validateNumber}>
+            Validate
+          </button>
+        </div>
+        ) : null
+      }
+      <div>
+
+        <button
+          onClick={switchEditNum}>
+          {labelSwitch}
+        </button>
+
+      </div>
+    </div>
+    )
+}
+export default Note;
