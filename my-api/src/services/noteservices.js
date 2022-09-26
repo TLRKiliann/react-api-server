ddbcac913f4cc8a1dfd520dd842e8fef88d3fad8
TLRKiliann <philogenie@protonmail.com>
2022-09-26 19:52:15 +0200

    switched on server-json

--- my-api/src/services/noteservices.js
+++ my-api/src/services/noteservices.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-//const baseUrl = 'http://localhost:4000/api/notes';
+//const baseUrl = 'http://localhost:4001/api/notes';
 const baseUrl = 'http://localhost:3001/notes';
 
 const getAll = () => {
